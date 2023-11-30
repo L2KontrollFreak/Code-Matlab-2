683a749e29724f4fb86b4e462cd5c982dbd95968
L2KontrollFreak <93939382+L2KontrollFreak@users.noreply.github.com>
2023-11-30 11:52:45 -0800

    Fix small errors like period and commas

--- DM2.m
+++ DM2.m
@@ -12,27 +12,27 @@ R = sqrt(X.^2 + Y.^2) + eps;
 Z = sin(R)./R;
 figure(3)
 mesh(X,Y,Z,'EdgeColor','black')
-surf(X.Y,Z)
+surf(X,Y,Z)
 colormap hsv 
 colorbar
 
 % problem 3 - if/else statement and for-loop
 D = [125 190 0 0 0] % (actual) dostance traveld in [cm] centermeter
 W = 125 % weight of vehicles in [grams]
-opexp = zerio(1,5) % operating expense, creates [0 0 0 0 0]; units are in[$]
+opexp = zeros(1,5) % operating expense, creates [0 0 0 0 0]; units are in[$]
 length(D) % length () calcuates the length
 
 for i=1:length(D)
-    if 50 > D(i) && D(i) > 0
+    if 50 > D(i) & D(i) > 0
         D(i) = 0
         opexp(i) = 0.003*W*D(i)+45
-    elseif 100 > D(i) && D(i) >= 50
+    elseif 100 > D(i) & D(i) >= 50
         D(i) = 50
         opexp(i) = 0.003*W*D(i)+45
-    elseif 150 > D(i) && D(i) >= 100
+    elseif 150 > D(i) & D(i) >= 100
         D(i) = 100
         opexp(i) = 0.003*W*D(i)+45
-    elseif 200 > D(i) && D(i) >= 150
+    elseif 200 > D(i) & D(i) >= 150
         D(i) = 150
         opexp(i) = 0.003*W*D(i)+45
     elseif D(i) >= 250
@@ -43,7 +43,7 @@ end
 
 % Problem 4 - While loop
 X = 5;
-while x < 25;
+while x < 25
     disp(x)
     x = 2*x - 1;
 end
